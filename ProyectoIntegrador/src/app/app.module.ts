c7e0d1cbc3f69095c62aaead93cced5cdf880161
Monse <monserratgutierrrez@gmail.com>
2022-07-23 00:40:00 -0300

    actual

--- ProyectoIntegrador/src/app/app.module.ts
+++ ProyectoIntegrador/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { RedesComponent } from './components/redes/redes.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercadeComponent } from './components/acercade/acercade.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    RedesComponent,
+    BannerComponent,
+    AcercadeComponent
   ],
   imports: [
     BrowserModule,
---
+++ ProyectoIntegrador/src/app/components/logo-ap/logo-ap.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-ap',
+  templateUrl: './logo-ap.component.html',
+  styleUrls: ['./logo-ap.component.scss']
+})
+export class LogoAPComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
