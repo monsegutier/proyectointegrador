a2367ececd3a498ba0e62e54ba96fd78d8a6d694
monsegutier <monserratgutierrrez@gmail.com>
2022-07-31 23:05:18 -0300

    .

--- Front End/src/app/app.module.ts
+++ Front End/src/app/app.module.ts
@@ -15,6 +15,7 @@ import {HttpClientModule} from '@angular/common/http';
 import { HomeComponent } from './componentes/home/home.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { FormsModule } from '@angular/forms';
+import { interceptorProvider } from './service/interceptor-service';
 
 @NgModule({
   declarations: [
@@ -37,7 +38,9 @@ import { FormsModule } from '@angular/forms';
     HttpClientModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    interceptorProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
